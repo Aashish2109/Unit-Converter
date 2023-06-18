a1e76530d3ee662932d821459e215c2d6e87f65e
Aashish Gupta <aashishg863@gmail.com>
2023-06-18 16:57:40 +0530

    Unit Converter

---
+++ script.js
@@ -0,0 +1,51 @@
+//Length Converter
+var feet = document.getElementById('Feet');
+var inch = document.getElementById('Inches');
+
+feet.addEventListener('input', function () {
+    let f = this.value;
+    let i = f * 12;
+    inch.value = i;
+});
+
+inch.addEventListener('input', function () {
+    let i = this.value;
+    let f = i / 12;
+    if (!Number.isInteger(f)) {
+        f=f.toFixed(2);
+    };
+    feet.value = f;
+});
+
+//Temperature Converter
+var Celsius = document.getElementById('Celsius');
+var Fahrenheit = document.getElementById('Fahrenheit');
+
+Celsius.addEventListener('input', function () {
+    let c = this.value;
+    let f = (9 / 5) * c + 32;
+    Fahrenheit.value = f;
+});
+
+Fahrenheit.addEventListener('input', function () {
+    let f = this.value;
+    let c = (5 / 9) * (f - 32);
+    Celsius.value = c;
+});
+
+//Volume Converter
+var litre = document.getElementById('Litre');
+var mL = document.getElementById('MLitre');
+
+litre.addEventListener('input', function () {
+    let L = this.value;
+    let ml = L * 1000;
+    mL.value = ml;
+});
+mL.addEventListener('input', function () {
+    let ml = this.value;
+    let L = ml / 1000;
+    litre.value = L;
+});
+
+
